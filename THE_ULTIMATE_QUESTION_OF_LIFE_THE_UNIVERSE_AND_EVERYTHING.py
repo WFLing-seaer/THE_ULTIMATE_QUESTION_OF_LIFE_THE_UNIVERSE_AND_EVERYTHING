42d1a02257413fdc2be90ac737fa00478d2e4b3c
WFLing-seaer <WFLing_seaer@163.com>
2026-08-12 21:55:29 +0800

    42?

--- THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING.py
+++ THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING.py
@@ -1,1 +1,29 @@
-ANSWER = 42
+import sys
+import types
+
+
+class YouCantKnowTheQuestion(Exception):
+    pass
+
+
+y = YouCantKnowTheQuestion(
+    "it seems that the Question and the Answer would just cancel each other out and take the Universe with them, which would then be replaced by something even more bizarrely inexplicable."
+)
+
+
+class TheQuestion(types.ModuleType):
+    def __str__(self):
+        raise y
+
+    def __repr__(self):
+        raise y
+
+    ANSWER = 42
+
+
+orig = sys.modules[__name__]
+mod = TheQuestion(__name__)
+mod.__file__ = orig.__file__
+mod.__package__ = orig.__package__
+mod.__loader__ = orig.__loader__
+sys.modules[__name__] = mod
